0c3a97112f7b809a2e19420019db08a35216039c
Gene Ng <fishballium@gmail.com>
2018-12-10 15:38:08 +0800

    Simplify partitioner logic to add partitions only

--- partitioner/src/handler.js
+++ partitioner/src/handler.js
@@ -1,64 +1,7 @@
 'use strict';
 
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 const athena = new AWS.Athena({apiVersion: '2017-05-18'});
-
-const copyObject = async (bucket, key, source) => {
-  const params = {
-    Bucket: bucket,
-    Key: key,
-    CopySource: source
-  };
-
-  if (process.env.NODE_ENV === 'dev') {
-    console.info(`Calling s3.copyObject with: ${JSON.stringify(params)}`);
-  }
-
-  const result = await s3.copyObject(params).promise();
-  if (process.env.NODE_ENV === 'dev') {
-    console.info(`s3.copyObject result: ${JSON.stringify(result)}`);
-  }
-  return result;
-}
-
-const isS3DirExist = async (bucket, prefix) => {
-  const params = {
-    Bucket: process.env.BUCKET_NAME,
-    Prefix: prefix,
-    MaxKeys: 1
-  };
-
-  if (process.env.NODE_ENV === 'dev') {
-    console.info(`Calling s3.listObjectsV2 with: ${JSON.stringify(params)}`);
-  }
-  const result = await s3.listObjectsV2(params).promise();
-  if (process.env.NODE_ENV === 'dev') {
-    console.info(`s3.listObjectsV2 result: ${JSON.stringify(result)}`);
-  }
-  return result.KeyCount > 0;
-}
-
-const repairTable = async (database, table, outputLoaction) => {
-  const params = {
-    QueryString: `MSCK REPAIR TABLE ${table}`,
-    ResultConfiguration: {
-      OutputLocation: outputLoaction
-    },
-    QueryExecutionContext: {
-      Database: database
-    }
-  }
-
-  if (process.env.NODE_ENV === 'dev') {
-    console.info(`Calling athena.startQueryExecution with: ${JSON.stringify(params)}`);
-  }
-  const result = await athena.startQueryExecution(params).promise();
-  if (process.env.NODE_ENV === 'dev') {
-    console.info(`athena.startQueryExecution result: ${JSON.stringify(result)}`);
-  }
-  return result;
-}
 
 module.exports.main = async (event) => {
   const bucket = process.env.BUCKET_NAME;
@@ -82,24 +25,29 @@ module.exports.main = async (event) => {
       filename
     ] = record.s3.object.key.split('/');
 
-    // Check if directory exists
-    const targetDir = `${targetPrefix}/partition_date=${year}-${month}-${day}/partition_hour=${hour}`;
-    const dirExist = await isS3DirExist(bucket, targetDir);
-
-    // Upload to S3
-    const targetPath = `${targetDir}/${filename}`;
-    const copySource = `/${bucket}/${source}`;
-    await copyObject(bucket, targetPath, copySource);
-
-    // Repair partition if created new directory in S3
-    if (!dirExist) {
-      console.info(`Auto creating new paritions ...`);
-      const outputLoaction = `s3://${bucket}/athena/repair_table`;
-      await repairTable(database, table, outputLoaction);
+    // Add partition for new events
+    const query = `
+    ALTER TABLE ${table} ADD IF NOT EXISTS
+      PARTITION (year = '${year}', month = '${month}', day = '${day}', hour = '${hour}')
+      LOCATION 's3://${bucket}/${prefix}/${year}/${month}/${day}/${hour}'
+    `;
+    const outputLoaction = `s3://${bucket}/athena/add_partition`;
+    const params = {
+      QueryString: query,
+      ResultConfiguration: {
+        OutputLocation: outputLoaction
+      },
+      QueryExecutionContext: {
+        Database: database
+      }
     }
 
     if (process.env.NODE_ENV === 'dev') {
-      console.info(`Done processing source object ${source}`);
+      console.info(`Calling athena.startQueryExecution with: ${JSON.stringify(params)}`);
+    }
+    const result = await athena.startQueryExecution(params).promise();
+    if (process.env.NODE_ENV === 'dev') {
+      console.info(`athena.startQueryExecution result: ${JSON.stringify(result)}`);
     }
   }));
 }
